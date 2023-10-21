3457a7597faa2cd26bd8d4de77013f623a08973d
mg12 <volibear1001@gmail.com>
2023-10-22 03:14:05 +0900

    a

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 const { signFromChainID } = require('../controller/payment');
-const { signFromChainID2 } = require('../controller/payment2');
 var router = express.Router();
 
 /* GET home page. */
@@ -9,21 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 
-// router.get('/payment', async function(req, res, next) {
-//   const tokenAddress = req.query.tokenAddress;
-//   const amount = req.query.amount;
-//   const targetChainId = req.query.targetChainId;
-//   console.log("checking params >>", tokenAddress,amount,targetChainId);
-//   const txhash = await signFromChainID(
-//     tokenAddress,
-//     amount,
-//     targetChainId 
-//   );
-//   res.send(txhash);
-// });
 router.get('/hi', async function(req, res, next) {
-  await signFromChainID2();
-  res.send("hi");
+  const tokenAddress = req.query.tokenAddress;
+  const amount = req.query.amount;
+  const targetChainId = req.query.targetChainId;
+  console.log("checking params >>", tokenAddress,amount,targetChainId);
+  const txhash = await signFromChainID(
+    tokenAddress,
+    amount,
+    targetChainId 
+  );
+  res.send("txhash");
 });
 //127.0.0.1:3000/payment?tokenAddress=0xe432143214&amount=123&targetChainId=1
 
