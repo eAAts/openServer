70857986b42dd750683d6cf0b0e5bb9b7787002f
mg12 <volibear1001@gmail.com>
2023-10-22 02:50:17 +0900

    hi

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,21 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/payment', async function(req, res, next) {
-  const tokenAddress = req.query.tokenAddress;
-  const amount = req.query.amount;
-  const targetChainId = req.query.targetChainId;
-  console.log("checking params >>", tokenAddress,amount,targetChainId);
-  const txhash = await signFromChainID(
-    tokenAddress,
-    amount,
-    targetChainId 
-  );
-  res.send(txhash);
-});
-router.get('/payment2', async function(req, res, next) {
+// router.get('/payment', async function(req, res, next) {
+//   const tokenAddress = req.query.tokenAddress;
+//   const amount = req.query.amount;
+//   const targetChainId = req.query.targetChainId;
+//   console.log("checking params >>", tokenAddress,amount,targetChainId);
+//   const txhash = await signFromChainID(
+//     tokenAddress,
+//     amount,
+//     targetChainId 
+//   );
+//   res.send(txhash);
+// });
+router.get('/hi', async function(req, res, next) {
   await signFromChainID2();
+  res.send("hi");
 });
 //127.0.0.1:3000/payment?tokenAddress=0xe432143214&amount=123&targetChainId=1
 
