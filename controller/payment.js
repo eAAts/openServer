d8a1e0975794daa83bfc46aa68078620238ca9c5
mg12 <volibear1001@gmail.com>
2023-10-22 01:28:34 +0900

    test

--- controller/payment.js
+++ controller/payment.js
@@ -1,18 +1,5 @@
 const fs = require('fs');
 
-// 파일에 쓸 내용
-const content = 'Hello, this is the content of the file.';
-
-// 파일 생성 및 쓰기
-fs.writeFile('example.txt', content, 'utf8', (err) => {
-  if (err) {
-    console.log('An error occurred:', err);
-    return;
-  }
-
-  console.log('File has been created and written successfully.');
-});
-
 async function signSomeTx_CHAIN1() {
     //signning tx here
     console.log("signTx")
@@ -38,16 +25,29 @@ async function signSomeTx_CHAIN4() {
 }
 
 async function signFromChainID(tokenAddress, amount, chainId) {
-    if(chainId==1) {
+    // 파일에 쓸 내용
+    const content = 'Hello, this is the content of the file.';
+
+    // 파일 생성 및 쓰기
+    fs.writeFile(`example${chainId}.txt`, content, 'utf8', (err) => {
+        if (err) {
+            console.log('An error occurred:', err);
+            return;
+        }
+
+        console.log('File has been created and written successfully.');
+    });
+
+    if (chainId == 1) {
         const resTx = await signSomeTx_CHAIN1();
         return resTx;
-    } else if (chainId==2){
+    } else if (chainId == 2) {
         const resTx = await signSomeTx_CHAIN2();
         return resTx;
-    } else if (chainId==3){
+    } else if (chainId == 3) {
         const resTx = await signSomeTx_CHAIN3();
         return resTx;
-    } else if (chainId==4){
+    } else if (chainId == 4) {
         const resTx = await signSomeTx_CHAIN4();
         return resTx;
     }
