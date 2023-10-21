deb1da5b6b7d14cbbf6593494c61e819af1b3c6d
hyunkicho <dmsrlgusrl@naver.com>
2023-10-21 15:42:09 +0900

    build: add open api for chainlink functions

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { signFromChainID } = require('../controller/payment');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,10 +7,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/payment', function(req, res, next) {
-  console.log(req.query.testParam)
-  const testResponse = req.query.testParam;
-  res.send(testResponse);
+
+router.get('/payment', async function(req, res, next) {
+  const tokenAddress = req.query.tokenAddress;
+  const amount = req.query.amount;
+  const targetChainId = req.query.targetChainId;
+  console.log("checking params >>", tokenAddress,amount,targetChainId);
+  const txhash = await signFromChainID(
+    tokenAddress,
+    amount,
+    targetChainId 
+  );
+  res.send(txhash);
 });
+//127.0.0.1:3000/payment?tokenAddress=0xe432143214&amount=123&targetChainId=1
 
 module.exports = router;
