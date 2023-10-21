c7395f5e64483fe9cd62e06a421aa4c9c68299fb
mg12 <volibear1001@gmail.com>
2023-10-22 01:23:13 +0900

    test

--- controller/payment.js
+++ controller/payment.js
@@ -1,3 +1,18 @@
+const fs = require('fs');
+
+// 파일에 쓸 내용
+const content = 'Hello, this is the content of the file.';
+
+// 파일 생성 및 쓰기
+fs.writeFile('example.txt', content, 'utf8', (err) => {
+  if (err) {
+    console.log('An error occurred:', err);
+    return;
+  }
+
+  console.log('File has been created and written successfully.');
+});
+
 async function signSomeTx_CHAIN1() {
     //signning tx here
     console.log("signTx")
